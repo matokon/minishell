b19e293dba99e32947f2ee2a06f68a4c9db9f517
nimboska <nimboska@gmail.com>
2025-08-29 17:54:27 +0200

    changed pars&exec

--- read_input.c
+++ read_input.c
@@ -2,19 +2,29 @@
 
 static int parse_and_execute(char *input, t_shell *shell)
 {
-  char  **arr;
-  t_token *list;
+	char  **arr;
+	t_token *list;
 
-    if (!input || !*input)
+	if (!input || !*input)
 		return (0);
-    input = deal_with_quotes(input, *shell);
-    arr = split_input_to_tokens(input);
-    //tutaj bedzie wywolana funckja parsujaca input do struktury t_cmd
-    list = token_list(arr);
-    shell->cmds = adding_command(list, shell);
-    if (!shell->cmds || shell->count_cmds == 0 || !(&shell->cmds[0]))
-      return (cmds_free(shell), 0);
-    return (0);
+	input = deal_with_quotes(input, *shell);
+	arr = split_input_to_tokens(input);
+	list = token_list(arr);
+	shell->cmds = adding_command(list, shell);
+	if (!shell->cmds || shell->count_cmds == 0 || !(&shell->cmds[0]))
+		return (cmds_free(shell), 0); //zwalnianie pamieci w przypadku bledu
+	if (shell->count_cmds == 1 &&
+				shell->cmds->argv &&
+				is_builtin(shell->cmds->argv[0]))
+		{
+			int st = run_single_builtin(shell);//uruchamiane bez forka na rodzicu
+			shell->last_status = st;
+			return st;
+		}
+		else
+		{
+			// return run_pipeline_or_external(shell);
+		}
 }
 
 int read_input(t_shell *shell)
