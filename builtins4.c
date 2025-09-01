253ec0b8b77fb81157820836d3464e63238ca400
Mateusz Okoń <mateusz.okon04@gmail.com>
2025-09-01 17:51:03 +0200

    added ft_export(not working)

---
+++ builtins4.c
@@ -0,0 +1,19 @@
+#include "mini.h"
+
+
+int	ft_export(t_shell *sh, char **argv)
+{
+	int	i;
+	int	code;
+
+	if (!argv || !argv[1])
+		return (print_sorted_export(sh->env));//TODO mokon
+	code = 0;
+	i = 0;
+	while (argv[++i])
+	{
+		if (handle_arg(sh, argv[i]) != 0)//TODO mokon:(
+			code = 1;
+	}
+	return (code);
+}
