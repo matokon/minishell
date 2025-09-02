7a53b77e887ea3bf5e2b73f9215470cafa183dc6
Mateusz Okoń <mateusz.okon04@gmail.com>
2025-09-02 15:20:22 +0200

    minor improvements across Makefile and free_func

--- builtins4.c
+++ builtins4.c
@@ -1,19 +1,19 @@
 #include "mini.h"
 
+//NIE GOTOWE
+// int	ft_export(t_shell *sh, char **argv)
+// {
+// 	int	i;
+// 	int	code;
 
-int	ft_export(t_shell *sh, char **argv)
-{
-	int	i;
-	int	code;
-
-	if (!argv || !argv[1])
-		return (print_sorted_export(sh->env));//TODO mokon
-	code = 0;
-	i = 0;
-	while (argv[++i])
-	{
-		if (handle_arg(sh, argv[i]) != 0)//TODO mokon:(
-			code = 1;
-	}
-	return (code);
-}
+// 	if (!argv || !argv[1])
+// 		return (print_sorted_export(sh->env));//TODO mokon
+// 	code = 0;
+// 	i = 0;
+// 	while (argv[++i])
+// 	{
+// 		if (handle_arg(sh, argv[i]) != 0)//TODO mokon:(
+// 			code = 1;
+// 	}
+// 	return (code);
+// }
--- mini_init.c
+++ mini_init.c
@@ -5,27 +5,23 @@ void set_path(t_shell *shell)
     char *current_path;
 
     if (!shell)
-		return ;
+        return;
+
     current_path = getcwd(NULL, 0);
     if (!current_path)
-	{
-		printf("Error: path problem!");
-		shell->exit_status = 1;
-        return ;
-	}
+    {
+        perror("minishell: getcwd");
+        shell->exit_status = 1;
+        return;
+    }
     if (shell->path)
-		free(shell->path);
-    shell->path = ft_strdup(current_path);
-    free(current_path);
-    if (!shell->path)
-	{
-		free(current_path);
-		shell->exit_status = 1;
-		return ;
-	}
-    update_env_val(shell->env, "PWD", shell->path);
-	shell->exit_status = 0;
+        free(shell->path);
+    shell->path = current_path;
+    update_env_val(&shell->env, "PWD", shell->path);
+
+    shell->exit_status = 0;
 }
+
 
 void value_init(t_shell *shell)
 {
